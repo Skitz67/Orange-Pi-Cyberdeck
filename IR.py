192d86af0caecd0b25835363f3a002c744f665d2
Skitz67 <tyronhollowayhill2@gmail.com>
2024-06-20 22:02:48 +1000

    IR Functions Beginning
    began making ir functions, currently spam should in theory be functional with spamming on and  off with editable delay.

--- IR.py
+++ IR.py
@@ -1,1 +1,33 @@
-#Commit Record baby
+import time
+import logging
+from OPIGPIO.opi_gpio import OPIGPIO
+
+# Initialize logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize the GPIO class
+gpio = OPIGPIO()
+
+# Run any setup code
+gpio.setup()
+
+#Set pin variables
+IRDATA = 12
+# Set the mode of pin 12 as output
+gpio.pinMode(IRDATA, "out")
+
+
+#ir spamming function
+def Spam(reps=1, delay=1):
+    for i in range(reps):
+        gpio.digitalWrite(IRDATA, 1)
+        time.sleep(delay)
+        gpio.digitalWrite(IRDATA, 0)
+
+def OnSig():
+    #yea work in progress
+    print("work in progress")
+    
+def OffSig():
+    #yea work in progress
+    print("work in progress")
