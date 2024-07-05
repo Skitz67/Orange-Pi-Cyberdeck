6e644535f23b784996b9b7e862978bd19bafc893
Skitz67 <tyronhollowayhill2@gmail.com>
2024-07-05 23:00:34 +1000

    magnet holes to case
    spaces for 5mm diameter 3mm tall cylinder magnets to be added in the corners mounted with glue.

--- USB.py
+++ USB.py
@@ -12,6 +12,7 @@ def WriteBadUsb(appName, label, icon, usbName, fileSuffix):
 
 def GrabData():
     print("grabbing data")
+    
     #rip info from attached external drive as fast as possible for sharing capabilities
 
-WriteBadUsb("appname.exe", "testLabel", "icon.ico", "usbname", ".exe")
+#WriteBadUsb("appname.exe", "testLabel", "icon.ico", "usbname", ".exe")
